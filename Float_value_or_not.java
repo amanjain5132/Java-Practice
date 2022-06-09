7806f6b274b5f9eaacdf0912252fcbd38b241f51
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-06-09 12:29:17 +0530

    Create Float_value_or_not.java

---
+++ Float_value_or_not.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Flot_value_or_not{
+    public static void main(String[] args){
+System.out.println("Enter your Number");
+         System.out.println(sc.hasNextFloat());
+    }
+    
+}
