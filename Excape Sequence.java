ce58c44c370b7d8d3f84521dec5ccc7acf4144d4
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-06-10 08:35:24 +0530

    Create Excape Sequence.java

---
+++ Excape Sequence.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Escape_Sequence_Characters
+{
+    public static void main(String[] args) 
+    {
+        //Escape Sequence Characters
+         System.out.println("I am escape sequence \n Double quote "); //\n
+         System.out.println("I am escape sequence \t Double quote "); //\t
+         System.out.println("I am escape sequence \' Double quote "); //\'
+         System.out.println("I am escape sequence \" Double quote "); //\""
+         System.out.println("I am escape sequence \\ Double quote "); //\\
+    }
+}
