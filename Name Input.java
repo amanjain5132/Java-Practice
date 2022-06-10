dc9a14ffd65c73aecb8d15c1cde359eb51baea6c
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-06-10 08:36:39 +0530

    Create Name Input.java

---
+++ Name Input.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Name_input{
+    public static void main(String[] args){
+      System.out.println("What is your name:");
+      String Name = sc.next();
+      System.out.println("Hello"  +Name+" have a good day!");   
+    }
+}
