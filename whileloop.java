995144d4f71df04cc6fc30f3549d1a95a774f2fa
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-06-17 07:44:31 +0530

    Create whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+public class cwh_21_ch5_loops
+{
+    public static void main(String[] args)
+    {
+        int i = 1;
+        while(i<=3)
+        {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish Running while loop");
+       // while (true)
+        {
+           // System.out.println("I am an Infinite While Loop !");
+        }
+    }
+}
