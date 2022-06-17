9bebc7da61720ec53fcd8b83e6e7dc80b78fabad
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-06-17 07:59:52 +0530

    Create do_while.java

---
+++ do_while.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class do_while
+{
+    public static void main(String[] args)
+    {
+        System.out.println("n NAtural Numbers code");
+        int a = 1;
+        do
+            {
+                System.out.println(a);
+                a++;
+            }
+        while (a<10);
+    }
+}
