731ab26fba0e191d8a566068fe6e5fea1b95897e
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-06-17 08:14:56 +0530

    Create for.java

---
+++ for.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class forloop
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Code for FOR LOOP");
+        for (int i = 0; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
+
