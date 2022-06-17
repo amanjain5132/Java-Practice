878cc4794b4a1986fb161ced18ba91bcbac5d617
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-06-17 08:16:58 +0530

    Create odd_for.java

---
+++ odd_for.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class forloop {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            System.out.println(2 * i + 1);
+        }
+    }
+}
+
